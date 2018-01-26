4a0428d178d6daadf329e585caf2a0f4e52a4dfd
Harry Dao <hoang.ktxd@outlook.com>
2018-01-26 15:18:52 +0700

    Foreach

--- Collection/Program.cs
+++ Collection/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,35 @@ namespace Collection
     {
         static void Main(string[] args)
         {
+            int[] intArray;
+            Employee[] empArray;
+            intArray = new int[5];
+            empArray = new Employee[3];
+
+            for(int i=0; i<empArray.Length; i++)
+            {
+                empArray[i] = new Employee(i + 5);
+            }
+            
+            for(int i=0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i].ToString());
+            }
+
+            for(int i=0; i < empArray.Length; i++)
+            {
+                Console.WriteLine(empArray[i].ToString());
+            }
+
+            Console.ReadKey();
         }
+    }
+
+    public class Employee
+    {
+        private int empId;
+        private int size;
+        public Employee(int empId) { this.empId = empId; }
+        public override string ToString() { return empId.ToString(); }
     }
 }
