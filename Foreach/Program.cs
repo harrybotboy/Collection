ea1b2084aed53abf89d5d7866e72cc2aa5ab620a
Harry Dao <hoang.ktxd@outlook.com>
2018-01-26 15:47:32 +0700

    Ví dụ về trao array như một thông số

--- Foreach/Program.cs
+++ Foreach/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace Foreach
 {
+    public class Employee
+    {
+        private int empId;
+        private int size;
+        public Employee(int empId) { this.empId = empId; }
+        public override string ToString() { return empId.ToString(); }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            int[] intArray;
+            Employee[] empArray;
+            intArray = new int[5];
+            empArray = new Employee[3];
+
+            for (int i = 0; i < empArray.Length; i++)
+            {
+                empArray[i] = new Employee(i + 5);
+            }
+
+            foreach(int i in intArray)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            foreach(Employee e in empArray)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ Para/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Para
+{
+    class Program
+    {
+        static void PrintArray(string[] myStrArray)
+        {
+            for(int i=0; i<myStrArray.Length; i++)
+            {
+                Console.WriteLine(myStrArray[i] + "{0}", i < myStrArray.Length - 1 ? "" : "");
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] conGiap = new string[] { "Tí", "Sửu", "Dần", "Mão", "Thìn", "Tỵ", "Ngọ", "Mùi", "Thân", "Tuất", "Dậu", "Hợi" };
+            PrintArray(conGiap);
+            Console.ReadKey();
+        }
+        
+    }
+}
